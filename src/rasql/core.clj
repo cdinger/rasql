9e2568b6a0da541f9dd617e2818cbc44464da10a
Chris Dinger <cdinger@gmail.com>
2014-10-02 13:17:44 -0500

    Implement basic joins and to-sql for predicates

--- src/rasql/core.clj
+++ src/rasql/core.clj
@@ -1,53 +1,70 @@
 (ns rasql.core
   (:require [clojure.string :as str]))
 
-;; (defn unique-alias
-;;   ([]
-;;     (unique-alias "asdf"))
-;;   ([relation-name]
-;;     (str/replace (str (java.util.UUID/randomUUID)) #"\-.*" "")))
+(defn wrap-parens [s]
+  (str "(" s ")"))
 
 (defprotocol IRelation
   (base [this])
   (alt [this])
   (sel [this])
-  (proj [this]))
+  (proj [this])
+  (joins [this]))
 
-(deftype Relation [base-relation relation-alias selection projection]
+(deftype Relation [base-relation relation-alias selection projection joins]
   IRelation
   clojure.lang.ILookup
   (base [_] base-relation)
   (alt [_] relation-alias)
   (sel [_] selection)
   (proj [_] projection)
+  (joins [_] joins)
   (valAt [_ k]
-    (str relation-alias "." (name k))))
+    (str (when-not (empty? relation-alias) (str relation-alias  ".")) (name k))))
 
 (defn project [relation columns]
   ;; TODO: join existing projected columns?
-  (Relation. (base relation) (alt relation) (sel relation) columns))
+  (Relation. (base relation) (alt relation) (sel relation) columns (joins relation)))
 
 (defn select [relation predicate]
-  (Relation. (base relation) (alt relation) predicate (proj relation)))
+  (Relation. (base relation) (alt relation) [:and (or (sel relation) [:= "1" "1"]) predicate] (proj relation) (joins relation)))
+
+(defn join [relation join-relation join-predicate]
+  ;; TODO: what checks are required on sels and projs?
+  (Relation. (base relation) (alt relation) (sel relation) (proj relation) {:relation join-relation
+                                                                            :predicate join-predicate}))
 
 (defmulti to-sql type)
 (defmethod to-sql String [relation] relation)
 (defmethod to-sql Relation [relation]
   (let [raw-cols (proj relation)
         cols     (if (empty? raw-cols) ["*"] (map #(name %) raw-cols))
-        alias    (alt relation)
-        where    (sel relation)]
-          (str "(SELECT " (str/join ", " cols)
-               " FROM " (to-sql (base relation))
-               (when-not (empty? where) (str " WHERE " where)) ")")))
+        alt      (alt relation)
+        where    (sel relation)
+        joins    (joins relation)]
+          (wrap-parens (str "SELECT " (str/join ", " cols)
+                            " FROM " (to-sql (base relation))
+                            (when-not (empty? joins)
+                              (str " JOIN " (to-sql (:relation joins)) " ON " (:predicate joins)))
+                            (when-not (empty? alt)
+                              (str " " alt))
+                            (when-not (empty? where) (str " WHERE " (to-sql where)))))))
+(defmethod to-sql clojure.lang.PersistentVector [predicate]
+  (let [operator (first predicate)
+        operands (vec (rest predicate))]
+    (if (contains? #{:and :or} operator)
+      (wrap-parens (str/join (str " " (str/upper-case (name operator)) " ") (map #(to-sql %) operands)))
+      (wrap-parens (str/join " " [(to-sql (to-sql (first operands)))
+                                  (name operator)
+                                  (to-sql (last operands))])))))
 
 (defn max [col]
   (str "max(" (name col) ")"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def ps-person (Relation. "ps_person" "p" nil nil))
-(def ps-names (Relation. "ps_names" "n" nil nil))
+(def ps-person (Relation. "ps_person" "p" nil nil {}))
+(def ps-names (Relation. "ps_names" "n" nil nil {}))
 (to-sql ps-person)
 (to-sql ps-names)
 
