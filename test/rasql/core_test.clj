19dabcdbce03c72f2d36f40c1c4fa5e8809b61be
Chris Dinger <cdinger@gmail.com>
2014-10-30 17:09:55 -0500

    Re-add tests

--- test/rasql/core_test.clj
+++ test/rasql/core_test.clj
@@ -1,100 +1,99 @@
 (ns rasql.core-test
   (:require [clojure.test :refer :all]
             [rasql.core :refer :all])
-  (:import (java.util GregorianCalendar)))
+  (:refer-clojure :exclude [comment]))
+
+(defrelation post :posts_tbl)
+(defrelation comment :comments_tbl)
+
+;; Projection
+
+(deftest projection-test
+  (let [p (->Projection [:a :b])
+       actual (to-sql p)
+       expected "SELECT a, b"]
+    (is (= expected actual))))
+
+(deftest empty-projection-test
+  (let [p (->Projection [])
+       actual (to-sql p)
+       expected "SELECT *"]
+    (is (= expected actual))))
+
+(deftest relation-projection-test
+  (let [p (->Projection [(:a post)])
+       actual (to-sql p)
+       expected "SELECT \"post\".a"]
+    (is (= expected actual))))
+
+;; Predicates
+
+(deftest predicate-test
+  (let [p [:= (:id post) (:post_id comment)]
+        actual (to-sql p)
+        expected "(\"post\".id = \"comment\".post_id)"]
+    (is (= expected actual))))
+
+
+(deftest relation-predicate-test
+  (let [p [:= (:a post) (:b post)]
+        actual (to-sql p)
+        expected "(\"post\".a = \"post\".b)"]
+    (is (= expected actual))))
+
+;; Joins
+
+(deftest join-test
+  (let [j (->Join comment [:= (:id post) (:post_id comment)])
+        actual (to-sql j)
+        expected " JOIN (SELECT * FROM comments_tbl \"comment\") \"comment\" ON (\"post\".id = \"comment\".post_id)"]
+    (is (= expected actual))))
 
 ;; Relation
 
-(deftest relation-alias-test
-  (let [r (new-relation "table" "x")]
-    (is (= (alt r) "x"))))
+(deftest relation-test
+  (let [actual (to-sql post)
+        expected "(SELECT * FROM posts_tbl \"post\")"]
+    (is (= expected actual))))
 
-(deftest basic-relation-test
-  (let [blah (new-relation "blah")]
-    (is (= (to-sql blah)
-           "(SELECT * FROM blah)"))))
+(deftest empty-project-test
+  (let [r (project post [])
+        actual (to-sql r)
+        expected "(SELECT * FROM posts_tbl \"post\")"]
+    (is (= expected actual))))
 
-(deftest project-one-test
-  (let [blah (project (new-relation "blah") [:a])]
-    (is (= (to-sql blah)
-           "(SELECT a FROM blah)"))))
+(deftest all-project-test
+  (let [r (project post [:*])
+        actual (to-sql r)
+        expected "(SELECT * FROM posts_tbl \"post\")"]
+    (is (= expected actual))))
 
-;; to-sql projection
+(deftest relation-all-project-test
+  (let [r (project post [(:* post)])
+        actual (to-sql r)
+        expected "(SELECT \"post\".* FROM posts_tbl \"post\")"]
+    (is (= expected actual))))
 
-(deftest project-mult-test
-  (let [blah (project (new-relation "blah") [:a :b :c])]
-    (is (= (to-sql blah)
-           "(SELECT a, b, c FROM blah)"))))
+(deftest multiple-project-test
+  (let [r (project post [:a :b :c])
+        actual (to-sql r)
+        expected "(SELECT a, b, c FROM posts_tbl \"post\")"]
+    (is (= expected actual))))
 
-(deftest project-relation-col-test
-  (let [some-table (new-relation "sometable")
-        blah (project (new-relation "blah") [(:somecol some-table)])]
-    (is (= (to-sql blah)
-           "(SELECT somecol FROM blah)"))))
+(deftest multiple-relation-column-project-test
+  (let [r (project post [(:a post) (:b post) :c])
+        actual (to-sql r)
+        expected "(SELECT \"post\".a, \"post\".b, c FROM posts_tbl \"post\")"]
+    (is (= expected actual))))
 
-(deftest project-aliased-relation-col-test
-  (let [t (new-relation "table" "t1")]
-    (is (= (to-sql (project t [(:id t)]))
-           "(SELECT t1.id FROM table t1)"))))
+(deftest relation-select-test
+  (let [r (select post [:= :title "An awesome post"])
+        actual (to-sql r)
+        expected "(SELECT * FROM posts_tbl \"post\" WHERE (title = 'An awesome post'))"]
+    (is (= expected actual))))
 
-;; to-sql selection
-
-(deftest select-single-test
-  (let [t (new-relation "table")]
-    (is (= (to-sql (select t [:= :id "123"]))
-           "(SELECT * FROM table WHERE (id = '123'))"))))
-
-(deftest select-single-table-col-test
-  (let [t (new-relation "table")]
-    (is (= (to-sql (select t [:= (:id t) "123"]))
-           "(SELECT * FROM table WHERE (id = '123'))"))))
-
-(deftest select-multiple-predicates-test
-  (let [t (new-relation "blah")
-        t' (select t [:= (:a t) "123"])
-        t'' (select t' [:= (:b t) 456])]
-    (is (= (to-sql t'')
-           "(SELECT * FROM blah WHERE ((a = '123') AND (b = 456)))"))))
-
-(deftest date-predicate-test
-  (let [d (.getTime (GregorianCalendar. 2014 0 14))]
-    (is (= (to-sql [:= :asdf d])
-           "(asdf = '2014-01-14')"))))
-
-(deftest predicate-value-first-test
-    (is (= (to-sql [:= 123 :id])
-        "(123 = id)")))
-
-;; join
-
-(deftest join-test
-  (let [p (new-relation "person" "p")
-        n (new-relation "names" "n")
-        p-n (join p n [:= (:emplid p) (:emplid n)])]
-    (is (= (to-sql p-n)
-           "(SELECT * FROM person p JOIN names n ON (p.emplid = n.emplid))"))))
-
-;; (deftest multi-join-test
-;;   (let [p (new-relation "person" "p")
-;;         n (new-relation "names" "n")
-;;         e (new-relation "email_addresses" "e")
-;;         p-n (join p n [:= (:emplid p) (:emplid n)])
-;;         p-n-e (join p-n e [:= (:emplid p-n) (:emplid e)])]
-;;     (is (= (to-sql p-n-e)
-;;            "(SELECT * FROM (SELECT * FROM person p JOIN names n ON (p.emplid = n.emplid)) p JOIN email_addresses e ON (p.emplid = e.emplid))"))))
-
-;; intersect
-
-(deftest intersect-test
-  (let [a (new-relation "a")
-        b (new-relation "b")]
-    (is (= (to-sql (intersect a b))
-           "((SELECT * FROM (SELECT * FROM a)) INTERSECT (SELECT * FROM b))"))))
-
-;; union
-
-(deftest union-test
-  (let [a (new-relation "a")
-        b (new-relation "b")]
-    (is (= (to-sql (union a b))
-           "((SELECT * FROM (SELECT * FROM a)) UNION (SELECT * FROM b))"))))
+(deftest relation-qualified-select-test
+  (let [r (select post [:= (:author_id post) 123])
+        actual (to-sql r)
+        expected "(SELECT * FROM posts_tbl \"post\" WHERE (\"post\".author_id = 123))"]
+    (is (= expected actual))))
