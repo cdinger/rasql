4a069ed763f59e75117513e1b7d9d1175393b279
Chris Dinger <ding0057@umn.edu>
2016-10-29 22:30:43 -0500

    Add group to project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rasql "0.1.0-SNAPSHOT"
+(defproject com.houseofding/rasql "0.1.0-SNAPSHOT"
   :description "A library for converting relational algebra expressions to SQL"
   :url "https://github.umn.edu/ding0057/rasql"
   :license {:name "Eclipse Public License"
