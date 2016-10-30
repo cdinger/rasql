f26b77b97a61b9d9d16ef289cb33f60b15437065
Chris Dinger <ding0057@umn.edu>
2016-10-29 22:32:14 -0500

    Bump clojure to 1.7

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.umn.edu/ding0057/rasql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
