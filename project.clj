3dc6740187f7740227638dc429894ab5faf65ee9
Chris Dinger <ding0057@umn.edu>
2016-11-01 16:41:48 -0500

    Use github.com URL
    Instead of private UMN URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.houseofding/rasql "0.2.0"
   :description "A library for converting relational algebra expressions to SQL"
-  :url "https://github.umn.edu/ding0057/rasql"
+  :url "https://github.com/cdinger/rasql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
